c4c0f9c41a5e1d106ee14be61a32b29bfed28e2f
Raychy <adesaint1@yahoo.com>
2024-09-20 00:47:12 +0100

    done with registration ui

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 
+import 'remixicon/fonts/remixicon.css';
+
 import { Toaster } from "@/components/ui/toaster";
 import { ThemeProvider } from "@/components/providers/Theme";
 
