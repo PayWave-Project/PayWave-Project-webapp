61f2891025b747f698ac9cfbe7f275fa73b518c8
Philip John <95682231+Philip-Nwabuwa@users.noreply.github.com>
2024-09-22 00:53:04 +0100

    fix: image build bug

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,21 +1,16 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
+import { Mulish } from "next/font/google";
 import "./globals.css";
 
-import 'remixicon/fonts/remixicon.css';
+import "remixicon/fonts/remixicon.css";
 
 import { Toaster } from "@/components/ui/toaster";
 import { ThemeProvider } from "@/components/providers/Theme";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
+const mulish = Mulish({
+  subsets: ["latin"],
   variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
+  weight: ["200", "300", "400", "500", "600", "700", "800", "900"],
 });
 
 export const metadata: Metadata = {
@@ -30,7 +25,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+      <body className={`${mulish.className}`}>
         <ThemeProvider attribute="class" defaultTheme="light" enableSystem>
           {children}
         </ThemeProvider>
